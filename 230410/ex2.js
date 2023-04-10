ceda50e9c572c3959d0ef4600c2a364f5013b22a
정승현 <armyso9506@plateer.com>
2023-04-10 15:58:16 +0900

    modify example

--- 230410/ex2.js
+++ 230410/ex2.js
@@ -1,5 +1,24 @@
-(
-    function( a, b ) {
-        console.log( a + b );
-    } ( 4, 5 )
-);
+var func = (
+    function () {
+        var num = 0
+
+        function increase () {
+            return ++num
+        }
+
+        function decrease () {
+            return --num
+        }
+
+        return {
+            increase: increase,
+            decrease: decrease,
+        }
+    } ()
+)
+
+console.log( func.num );
+console.log( func.increase() );
+console.log( func.decrease() );
+console.log( func.decrease() );
+console.log( func.decrease() );
