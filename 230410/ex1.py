8591554f9bfc644aa72c84cf352c9e38e464603b
정승현 <armyso9506@plateer.com>
2023-04-10 11:09:27 +0900

    add example

---
+++ 230410/ex1.py
@@ -0,0 +1,38 @@
+def one() :
+    questionList.append( input( '등록할 질문을 입력해주세요\n\n=> ' ) )
+    return
+
+def two() :
+    for index, question in enumerate( questionList ) :
+        print( '{}. {}'.format( index + 1, question ) )
+
+    answer = int(input( '\n\n위 질문들 중 답변할 질문을 선택해주세요\n\n=> ' ))    
+    
+        
+    return
+
+def three() :
+    return
+
+
+
+questionList = []
+
+
+
+while True :
+    try :
+        firstAnswer = int(input( '아래 보기에서 메뉴를 선택해주시기 바랍니다.\n1.질문등록\n2.답변하기\n3.질문보기\n\n=> ' ))
+    except :
+        print('\n\n\n\n')
+        continue
+
+    if firstAnswer == 1 :
+        one()
+    elif firstAnswer == 2 :
+        two()
+        break
+    elif firstAnswer == 3 :
+        three()
+        break
+    
