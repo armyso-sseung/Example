0151a3a352f1de4823229118aabd585402867101
정승현 <armyso9506@plateer.com>
2023-05-23 15:54:12 +0900

    Modify Example

--- 230516/ex1.py
+++ 230516/ex1.py
@@ -6,9 +6,10 @@ from lxml import html
 def unipass_api( hblNos ):
     for hblNo in hblNos :
         url = "https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?crkyCn=q220h213t025o176b020z010c0&hblNo={0}&blYy=2023".format(hblNo)
+        print(url)
         headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
 
-        response = requests.get(url).text.encode('utf-8')
+        response = requests.get(url).text
         xmlData = bs4.BeautifulSoup(response, 'lxml-xml')
         # print(xmlData)
         print("송장번호: " + hblNo)
@@ -22,19 +23,19 @@ def unipass_api( hblNos ):
 
 
 
-hblNos = ['WJ00001260616', 'WJ00001338415', 'WJ00001338751', '6079449163665', '6079449736486', '570776427326']
+hblNos = ['WJ00001260616', '6079530034012', '6079449736486', '570776427326']
 unipass_api(hblNos)
 
 
 
 '''
     N0000022808543(오링)                            -   반출완료
-    WJ00001338751(Princess Switch)                 -   미등록
-    WJ00001260616(8008 키캡)                        -   통관목록접수
-    WJ00001338415(보라색 키캡)                        -   미등록
-    WJ00001251433(GMK67)                           -   반출완료
-    6079449163665(LMK81)                           -   통관목록접수
-    6079449019909(CreamYellow Switch, 9009 키캡)    -   통관완료 
-    6079449736486(보라색 키캡)                        -   미등록
-    570776427326(Monsgeek M3)                      -   미등록
+    6079530034012(Princess Switch)                 -   미등록
+    WJ00001260616(8008 키캡)                        -   반입기간연장 승인
+    6079530034012(보라색 키캡)                        -   미등록
+    WJ00001251433(GMK67)                           -   반출완료(수령완료)
+    6079449163665(LMK81)                           -   반출완료
+    6079449019909(CreamYellow Switch, 9009 키캡)    -   반출완료(수령완료) 
+    6079449736486(보라색 키캡)                        -   사용소비 심사진행
+    570776427326(Monsgeek M3)                      -   하선신고 수리완료
 '''
